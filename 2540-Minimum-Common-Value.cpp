4d9ec5a7b3295270d5617dd38ed28844747ea8ec
Manish Shaw <manishshaw301106@gmail.com>
2026-08-08 18:43:53 +0530

    [2540] [Time Beats: 100.00%] [Memory Beats: 25.60%] - LeetPush

--- 2540-Minimum-Common-Value.cpp
+++ 2540-Minimum-Common-Value.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        set<int> s(nums1.begin(), nums1.end());
+        int i = 0, j = 0;
 
-        for (int x : nums2) {
-            if (s.count(x)) return x;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } 
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            } 
+            else {
+                j++;
+            }
         }
 
         return -1;
