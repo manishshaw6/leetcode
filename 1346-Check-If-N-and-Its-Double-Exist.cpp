77bd629833fc0e074fc3b7f68a255b611a6ef77b
Manish Shaw <manishshaw301106@gmail.com>
2026-08-11 21:33:03 +0530

    [1346] [Time Beats: 13.23%] [Memory Beats: 5.59%] - LeetPush

--- 1346-Check-If-N-and-Its-Double-Exist.cpp
+++ 1346-Check-If-N-and-Its-Double-Exist.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s(arr.begin(),arr.end());
+        int zcount=0;
         for(int i=0;i<arr.size();i++){
-            for(int j=i+1;j<arr.size();j++){
-                if(i!=j&&(arr[i]==arr[j]*2||arr[j]==arr[i]*2)){
+            if(arr[i]==0)zcount++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                if(zcount>1){
                     return true;
                 }
             }
+            else if(s.count(arr[i]*2))return true;
         }
         return false;
+        
     }
 };
